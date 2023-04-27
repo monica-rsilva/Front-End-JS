2c41a70b770202f3908c71204ae9837478842080
MÔNICA RIBEIRO DA SILVA <22282167@CFP127ALUNO.LOCAL>
2023-04-27 10:29:29 -0300

    aprendendo sobre roteamento

---
+++ roteamento/js/router.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const routes = {
+    '/': '/pages/home.html',
+    '/vermelho': '/pages/vermelho.html',
+    '/azul': '/pages/azul.html',
+}
+
+const route = async() => {
+    window.event.preventDefault()
+    window.history.pushState({}, "", window.event.target.href)
+
+    const path = window.location.pathname
+    const route = routes[path]
+
+    const response = await fetch(route)
+    const html = await response.text()
+
+    document.getElementById('root').innerHTML = html
+
+    // console.log(html)
+
+    // console.log(routes[path])
+
+    // console.log(window.event.target.href)
+}
+
+window.route = route
